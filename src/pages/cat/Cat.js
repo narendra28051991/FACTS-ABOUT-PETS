0327d013d31abff6035899e910f8fbbd9a33e494
narendra28051991 <narendra28051991@gmail.com>
2022-10-23 11:07:59 +0200

    cat page created

--- src/pages/cat/Cat.js
+++ src/pages/cat/Cat.js
@@ -1,10 +1,18 @@
+// routes
+import { Link } from 'react-router-dom';
+
 // styles
 import './Cat.css';
 
 export default function Cat() {
   return (
-    <div>
-      Cat
+    <div className='fact-list'>
+      <div className='fact'>
+        <h2>Cat Fact of the day</h2>
+        <h3>Fact</h3>
+        <p>Word length: </p>
+        <div><Link to='/cat'></Link></div>
+      </div>
     </div>
   )
 }
