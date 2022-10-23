9145a3b8f07671d702679bb57039b210b288e2a7
narendra28051991 <narendra28051991@gmail.com>
2022-10-23 11:44:19 +0200

    useFetch hook created

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+
+    useEffect(() => {
+        const abortController = new AbortController()
+        setIsPending(true)
+        const fetchData = async () => {
+
+            try {
+                const response = await fetch(url, { signal: abortController.signal })
+                if(!response.ok) {
+                    throw new Error(response.statusText)
+                }
+                const data = await response.json()
+                setData(data)
+                setIsPending(false)
+                setError(null)
+            }
+
+            catch(err) {
+                if(err.name === 'AbortError') {
+                    setError('The fetch was aborted')
+                }
+                else {
+                    setIsPending(false)
+                    setError('Could not fetch the data for the resource')
+                }
+            }
+        }
+
+        // invoke the function
+        fetchData()
+
+        return () => {
+            abortController.abort()
+        }
+    }, [url])
+
+    return { data, isPending, error }
+}
--- src/pages/cat/Cat.js
+++ src/pages/cat/Cat.js
@@ -1,18 +1,28 @@
 // routes
 import { Link } from 'react-router-dom';
 
+// hooks
+import { useFetch } from '../../hooks/useFetch';
+
 // styles
 import './Cat.css';
 
 export default function Cat() {
+  const url = 'https://catfact.ninja/fact'
+  const { data, isPending, error } = useFetch(url)
+
   return (
     <div className='fact-list'>
-      <div className='fact'>
-        <h2>Cat Fact of the day</h2>
-        <h3>Fact</h3>
-        <p>Word length: </p>
-        <div><Link to='/cat'></Link></div>
-      </div>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && (
+        <div className='fact'>
+          <h2>Cat Fact of the day</h2>
+          <h3>{data.fact}</h3>
+          <p>Word length: {data.length}</p>
+          <div><Link to='/cat'>New Fact</Link></div>
+        </div>
+      )}  
     </div>
   )
 }
